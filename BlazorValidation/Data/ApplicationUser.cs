5c6dbee957314eaa80f0e650201462509fb6d998
Michael VanMeter <MGVdec2000@gmail.com>
2022-11-04 21:07:50 -0400

    EditContext_OnFieldChanged is never raised

---
+++ BlazorValidation/Data/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorValidation;
+
+public class ApplicationUser : IdentityUser
+{
+    public bool MustChangePassword { get; set; } = false;
+
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; }
+}
---
+++ BlazorValidation/Data/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Newtonsoft.Json;
+
+namespace BlazorValidation.Data;
+
+public class Employee
+{
+    [Required]
+    [DisplayName("First Name")]
+    [JsonProperty(PropertyName = "firstName")]
+    public string FirstName { get; set; } = "";
+
+    [Required]
+    [DisplayName("Last Name")]
+    [JsonProperty(PropertyName = "lastName")]
+    public string LastName { get; set; } = "";
+
+    [Required]
+    [DisplayName("Email")]
+    [DataType(DataType.EmailAddress)]
+    [JsonProperty(PropertyName = "email")]
+    [EmailAddress]
+    public string Email { get; set; } = "";
+}
